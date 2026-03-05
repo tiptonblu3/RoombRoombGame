76fc2f4adf3a58f003a8b26f142509bedbe1145d
Jordon N Dubin <jordon.n.dubin@gmail.com>
2026-03-05 13:00:21 -0500

    Player movement setup

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 
 public class player : MonoBehaviour
@@ -12,8 +13,32 @@ public class player : MonoBehaviour
     [Header("Attack Stuff")]
     public float attackSpeed;
     public float IFrames;
-    
-    
+
+    [Header("Movement Settings")]
+    public Vector2 movementInput;
+    public Rigidbody rb;
+
+    private void OnMove(InputValue inputValue)
+    {
+        //grab values from unity input system (for both mediums) and set it to this variable
+        movementInput = inputValue.Get<Vector2>();
+
+        //Debug.Log(inputValue.Get<Vector2>()); to show a button was pressed and recieved for debugging purposes, can be removed later
+    }
+    private void FixedUpdate()
+    {
+        ManageMovement();
+    }
+
+
+    private void ManageMovement()
+    {
+        Vector3 moveDir = transform.right * movementInput.x + transform.forward * movementInput.y;
+
+        // Apply velocity while preserving existing gravity (y-axis)
+        rb.linearVelocity = new Vector3(moveDir.x * speed, rb.linearVelocity.y, moveDir.z * speed);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
