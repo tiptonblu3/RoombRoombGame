82aadae6822a9aad2a0de3196842d99988ed8bfb
Jman87 <juanjomartinezv2707@gmail.com>
2026-03-06 20:42:01 -0500

    just trying stuff

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -1,4 +1,7 @@
 using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
 
 public class enemy : MonoBehaviour
 {
@@ -8,20 +11,34 @@ public class enemy : MonoBehaviour
     public float defense;
     public float speed;
 
+    public NavMeshAgent EnemyMonster;
+    public Transform Player;
+
     [Header("Attack Stuff")]
     public float attackSpeed;
 
+    // Notes:
+    /*
+    1. Make parent enemy object (probably call it enemy spawner), and have all enemies spawn under it.
+    2. Spawner loop (might have to save that for game manager)
+    3. function for player taking damage, might have to do that in player
+    */
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log(EnemyMonster.isOnNavMesh);
+        EnemyMonster.stoppingDistance = 0f;
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Player != null && EnemyMonster != null)
+        {
+            EnemyMonster.SetDestination(Player.position);// immediately makes the enemy target the player and march towards them.
+        } 
     }
 }
